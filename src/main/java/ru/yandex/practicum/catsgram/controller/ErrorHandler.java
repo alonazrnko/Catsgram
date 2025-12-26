56fdc43f8b9022f8f0d0ecbd703ac5e4d06f259f
Alyona Nazarenko <alonazrnko@gmail.com>
2025-12-26 16:06:05 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.catsgram.exception.*;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(DuplicatedDataException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleDuplicated(DuplicatedDataException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ConditionsNotMetException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErrorResponse handleConditions(ConditionsNotMetException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(ParameterNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleParameter(ParameterNotValidException e) {
+        return new ErrorResponse(
+                "Некорректное значение параметра " + e.getParameter() + ": " + e.getReason()
+        );
+    }
+
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleOther(Throwable e) {
+        return new ErrorResponse("Произошла непредвиденная ошибка.");
+    }
+}
+
---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,39 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody User user) { return userService.create(user); }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        return userService.update(user);
+    }
+}
